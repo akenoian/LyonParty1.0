2d27d5372b559e412e314e71992357ef2bbfae33
Alex <kenoi22a@mtholyoke.edu>
2019-01-15 08:59:17 -0800

    update to 3 activities with xml

--- app/src/main/java/com/example/demouser/lyonparty/MainActivity.java
+++ app/src/main/java/com/example/demouser/lyonparty/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demouser.lyonparty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //open the postEvent screen
+        Intent intent = new Intent(this, SearchEvent.class);
+        //takes a string name and value
+        startActivity(intent);  //new UI
+
     }
 }
---
+++ app/src/main/java/com/example/demouser/lyonparty/SearchEvent.java
@@ -0,0 +1,13 @@
+package com.example.demouser.lyonparty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_event);
+    }
+}
