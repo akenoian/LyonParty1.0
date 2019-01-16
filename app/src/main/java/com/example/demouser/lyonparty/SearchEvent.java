84ee21740439b1ecc5244d405d4069be8f98a4f5
Jaemarie Solyst <solys22j@mtholyoke.edu>
2019-01-16 08:14:58 -0800

    added divider for events

--- app/src/main/java/com/example/demouser/lyonparty/SearchEvent.java
+++ app/src/main/java/com/example/demouser/lyonparty/SearchEvent.java
@@ -2,6 +2,7 @@ package com.example.demouser.lyonparty;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -16,6 +17,7 @@ import java.util.ArrayList;
 public class SearchEvent extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener{
 
     MyRecyclerViewAdapter adapter;
+    private LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +34,24 @@ public class SearchEvent extends AppCompatActivity implements MyRecyclerViewAdap
         ArrayList<EventNotice> notices = new ArrayList<>();
         notices.add(demo);
 
+        // instantiate layout manager
+        layoutManager = new LinearLayoutManager(this);
 
         // set up the RecyclerView
         RecyclerView recyclerView = findViewById(R.id.eventListView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(layoutManager);
         adapter = new MyRecyclerViewAdapter(this, notices);
         adapter.setClickListener(this);
         recyclerView.setAdapter(adapter);
 
+        // create division for events
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
     }
+
+
     @Override
     public void onItemClick(View view, int position) {
         //add an intent to open another activity that's just an xml of the event information
